89cee133dd920dc852f1a0b593bc52f75eff58ca
Amir Aslamov <amiraslamov@usf.edu>
2022-04-06 06:46:47 -0400

    Add files via upload

---
+++ WebScraperConsoleApp/obj/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WebScraperConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // we will create a List data structure of string type to store the returned data
+            // from the scraper object
+            List<string> fetched_data = new List<string>();
+
+            // this is the object from the scraper class we have created
+            ScrapingLogic scraper = new ScrapingLogic();
+
+            // a variable to store the url of a web page to be scraped
+            string url = "https://www.usf.edu/engineering/";
+
+            // we will use the method of the scraper to return the list of nodes' Inner Html
+            fetched_data = scraper.extractNodes(url);
+
+            File.WriteAllLines("./RESULTS_2.txt",
+                fetched_data);
+        }
+    }
+}
---
+++ WebScraperConsoleApp/obj/ScrapingLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+
+
+namespace WebScraperConsoleApp
+{
+    public class ScrapingLogic
+    {
+        public List<string> extractNodes(string url)
+        {
+            // a list to store nodes form a web page
+            List<string> extracted_nodes = new List<string>();
+
+            // an object DOM to crawl and extract data from web pages
+            HtmlWeb web = new HtmlWeb();
+
+            // the DOM loads a web pages from a url and stores in a variable
+            var HTML_doc = web.Load(url);
+
+            // we will specify what nodes from a web page we want to fetch data from
+            var nodes = HTML_doc.DocumentNode.SelectNodes("/html/body");
+
+            foreach(var node in nodes)
+            {
+                extracted_nodes.Add(node.InnerHtml);
+            }
+
+            return extracted_nodes;
+        }
+    }
+}
